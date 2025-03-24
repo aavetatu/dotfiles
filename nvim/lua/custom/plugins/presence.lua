13ad74c2a51c4b22803a2dee568b3e57f84d7de0
Tatu Heikkinen <52470440+aavetatu@users.noreply.github.com>
2025-03-24 23:49:20 +0200

    Edit presence.lua

--- nvim/lua/custom/plugins/presence.lua
+++ nvim/lua/custom/plugins/presence.lua
@@ -1,8 +1,6 @@
 return {
 	"andweeb/presence.nvim",
 	config = function()
-		-- The setup config table shows all available config options with their default values:
-		--
 		require("presence").setup({
 			-- General options
 			auto_update = true, -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
