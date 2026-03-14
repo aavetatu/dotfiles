5f05d474fb81027eed1722f260d7f397f5b7de3c
Tatu <52470440+aavetatu@users.noreply.github.com>
2026-03-14 03:47:53 +0200

    fix: remove treesitter bc skill issue (config breaks); update equibop path for stow

--- nvim/.config/nvim/lua/custom/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" },
-  config = function()
-    require("custom.treesitter")
-  end,
-}
-
