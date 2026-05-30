8713227e07a5b3248dc8a8a143afd9aed2ed246a
Tatu <52470440+aavetatu@users.noreply.github.com>
2026-05-30 23:49:39 +0300

    fix: update hyprland.conf to hyprland.lua

---
+++ hypr/.config/hypr/hyprland.lua
@@ -0,0 +1,10 @@
+require("hyprland.variables")
+require("hyprland.input")
+
+-- KEYBINDINGS
+require("hyprland.keybinds")
+require("hyprland.monitors")
+require("hyprland.workspaces")
+require("hyprland.autostart")
+require("hyprland.devices")
+require("hyprland.env")
