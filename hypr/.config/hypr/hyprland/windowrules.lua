97d9eb2e45717b0abaf8827f449f2cd5347d66d0
Tatu <52470440+aavetatu@users.noreply.github.com>
2026-07-07 18:57:50 +0300

    feat: modify opacity rules for kitty and equibop

--- hypr/.config/hypr/hyprland/windowrules.lua
+++ hypr/.config/hypr/hyprland/windowrules.lua
@@ -1,5 +1,9 @@
 hl.window_rule({
-	name = "opacity",
-	match = { fullscreen = false },
-	opacity = 0.95,
+	match = { class = "kitty" },
+	opacity = ".9 override .7 override .8",
+})
+
+hl.window_rule({
+	match = { class = "equibop" },
+	opacity = "1 .85",
 })
